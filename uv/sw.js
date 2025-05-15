1795844c35476d12fafb652169da8edf22853c18
Daniel Kingston <kizzobotwins@gmail.com>
2025-05-15 19:53:33 -0400

    sw.js change

--- uv/sw.js
+++ uv/sw.js
@@ -1,32 +1,11 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
-importScripts('uv.bundle.js');
-importScripts('uv.config.js');
-// importScripts(__uv$config.sw || 'uv.sw.js'); // REMOVED THIS LINE
-
-const uv = new ServiceWorker();
-
-self.addEventListener('install', (event) => { // Added install and activate events.
-  event.waitUntil(self.skipWaiting());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event));
-});
-
-async function handleRequest(event) {
-    if (uv.route(event)) {
-        return await uv.fetch(event);
-    }
-    
-    return await fetch(event.request)
-}
+self.addEventListener('install', (event) => {
+    event.waitUntil(self.skipWaiting());
+  });
+  
+  self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+  });  
